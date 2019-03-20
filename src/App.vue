cf4235337e825a6b5bd05d3e0d322bc49065b908
arashabedin <arash.abedin@gmail.com>
2019-03-20 19:36:57 +0100

    add footer

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,22 @@
   <div id="app">
     <NavBar/>
     <IntroPage msg="Welcome to Your Vue.js App"/>
-
+    <Footer/>
   </div>
 </template>
 
 <script>
 import IntroPage from './components/IntroPage.vue'
 import NavBar from './components/NavBar.vue'
+import Footer from './components/Footer.vue'
 
 
 export default {
   name: 'app',
   components: {
     IntroPage,
-    NavBar
+    NavBar,
+    Footer
   }
 }
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+
+
+<template>
+  
+<div class="container">
+<footer class="sixteen columns" style="margin-top:-10px;">
+		<hr />
+	<ul id="footerLinks">
+		<li>&copy; 2016 Arash Abedin</li>
+	  <li><a href="mailto:arash.abedin@gmail.com"> arash.abedin@gmail.com</a></li>
+	  <li><a href="http://arashabedin.blogspot.com" target="_blank">Blogger</a>
+	
+	  </li>
+	  <li>+45 60 52 60 31</li></ul>
+    </footer></div></div>
+
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  props: {
+  }
+}
+</script>
+
+
